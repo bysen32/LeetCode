51328926df3a4d20ad12ee1b94043f002c884e6c
bysen32 <bysen32@gmail.com>
2017-01-31 13:55:14 +0800

    update

--- Medium/473. Matchsticks to Square.py
+++ Medium/473. Matchsticks to Square.py
@@ -35,3 +35,35 @@ class Solution(object):
                 vis[i] = 1
                 self.dfs(nums, vis, numcnt+1, (rec + nums[i]) % self.edgeLen)
                 vis[i] = 0
+
+# 解法二
+class Solution(object):
+    def makesquare(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not nums or len(nums) < 4:
+            return False
+        if sum(nums) % 4 != 0:
+            return False
+        nums = sorted(nums, reverse=True)
+        sums = [0 for i in range(4)]
+        target = sum(nums) / 4
+        return self.dfs(nums, sums, 0, target)
+
+    def dfs(self, nums, sums, index, target):
+        if index == len(nums):
+            for s in sums:
+                if s != target:
+                    return False
+            return True
+
+        for i in range(len(sums)):
+            if sums[i] + nums[index] <= target:
+                sums[i] += nums[index]
+                if self.dfs(nums, sums, index + 1, target):
+                    return True
+                sums[i] -= nums[index]
+        return False
+
